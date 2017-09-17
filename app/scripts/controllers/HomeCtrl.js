7360e936f0740368fcbdc648d32a3853f3c318f5
Victoria Finney <vfinney2@gmail.com>
2017-09-17 00:32:11 -0400

    Send messages completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,29 +1,36 @@
 (function(){
-  function HomeCtrl (Room, Message, $cookies, $uibModal) {
-    this.currentRoom = null;
-    this.currentUser = $cookies.get('CurrentUser');
+  function HomeCtrl (Room, Message, $uibModal, $cookies) {
+    this.currentRoom = {};
+    this.currentUser = $cookies.get('ChatCurrentUser');
     this.rooms = Room.all;
+    this.newMessage = {};
+   
       
-        this.addRoom = function() {
-            $uibModal.open({
-                templateUrl: '/templates/modal.html',
-                size: 'sm',
-                controller: 'ModalCtrl as modal'
-            });
-          
-        }
-        
-        this.createCurrentRoom = function (room) {
-           this.currentRoom = room;
-           this.messages = Message.getByRoomId(this.currentRoom.$id);
-                      console.log(this.messages);
-        
-        }
+    this.addRoom = function() {
+        $uibModal.open({
+            templateUrl: '/templates/modal.html',
+            size: 'sm',
+            controller: 'ModalCtrl as modal'
+        });
+
+    }
+
+    this.createCurrentRoom = function (room) {
+       this.currentRoom = room;
+       this.messages = Message.getByRoomId(this.currentRoom.$id);
+       console.log(this.messages);
+    }
+
+    this.sendMessage = function(){
+      this.newMessage.roomId = this.currentRoom.$id;
+      this.newMessage.username = this.currentUser;
+      Message.send(this.newMessage);
+    }
 
   }
 
   angular
     .module('chatRoomProject')
-    .controller('HomeCtrl', ['Room', 'Message', '$cookies', '$uibModal', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$uibModal','$cookies', HomeCtrl]);
 })();
 
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,13 +1,19 @@
 (function() {
-     function Message($firebaseArray) {
+     function Message($firebaseArray, $cookies) {
          var Message = {};
          
          var ref = firebase.database().ref().child("messages");
          var messages = $firebaseArray(ref);
+         Message.all = messages;
          
          Message.getByRoomId = function (roomId) {
              return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
          }
+         
+        Message.send = function(newMessage) {
+            newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+            messages.$add(newMessage);
+    };
          
          return Message;
      }
@@ -16,3 +22,5 @@
          .module('chatRoomProject')
          .factory('Message', ['$firebaseArray', Message]);
  })();
+
+
